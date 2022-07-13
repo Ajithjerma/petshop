a4982f0cd9e72605c0bbc46555849ff33d77d4d8
Ajithjerma <85161662+Ajithjerma@users.noreply.github.com>
2022-07-13 16:45:44 +0530

    Add files via upload

---
+++ PetTechData/src/main/java/com/example/demo/PetTechDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTechDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTechDataApplication.class, args);
+	}
+
+}
